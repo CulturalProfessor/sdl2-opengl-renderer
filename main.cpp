3ffcf8c01e1f238fae47d57a28b61eddb8d16d9f
CulturalProfessor <svinayak994@gmail.com>
2026-07-01 22:57:07 +0530

    fix : nrChannels and format issue

--- main.cpp
+++ main.cpp
@@ -60,17 +60,27 @@ GLuint generateTexture(const char *imagePath) {
   glBindTexture(GL_TEXTURE_2D, texture);
   // set the texture wrapping/filtering options
   // (on the currently bound texture object)
+  glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
                   GL_LINEAR_MIPMAP_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   // load and generate the texture
-  int width, height, nrChannels;
+  int width, height, nrChannels, format = 1;
 
   unsigned char *data = stbi_load(imagePath, &width, &height, &nrChannels, 0);
+
+  if (nrChannels == 1) {
+    format = GL_RED;
+  } else if (nrChannels == 3) {
+    format = GL_RGB;
+  } else if (nrChannels == 4) {
+    format = GL_RGBA;
+  }
+
   if (data) {
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+    glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format,
                  GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
   } else {
@@ -433,8 +443,8 @@ int main() {
       glm::radians(45.0f), (float)gApp.mScreenWidth / (float)gApp.mScreenHeight,
       0.1f, 100.0f);
 
-  gMesh1.mTexture = generateTexture("../assets/wall.jpg");
-  gMesh2.mTexture = generateTexture("../assets/brick.jpg");
+  gMesh1.mTexture = generateTexture("../assets/brick.jpg");
+  gMesh2.mTexture = generateTexture("../assets/wall.jpg");
 
   meshCreate(&gMesh1);
   meshCreate(&gMesh2);
